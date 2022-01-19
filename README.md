3cd337c1716b7592a73dac0e3ad26365d0ac4f33
0xflotus <0xflotus@gmail.com>
2022-01-19 14:06:59 +0100

    adjustments

--- README.md
+++ README.md
@@ -5,13 +5,13 @@ A small library to convert IP addresses to integers
 ### Usage
 
 ```py
-from ipy2d import fun
+from ipy2d import convert
 
-print(fun.from_4("127.0.0.1")) # -> 2130706433
-print(fun.to_4(134744072)) # -> 8.8.8.8
+print(convert.from_4("127.0.0.1")) # -> 2130706433
+print(convert.to_4(134744072)) # -> 8.8.8.8
 
-print(fun.from_6("::1")) # -> 1
-print(fun.to_6(1051570404137199630024704)) # -> 0000:0000:0000:dead:beef:0000:0000:0000
+print(convert.from_6("::1")) # -> 1
+print(convert.to_6(1051570404137199630024704)) # -> 0000:0000:0000:dead:beef:0000:0000:0000
 ```
 
 #### Development
--- test/test_ipy2d.py
+++ test/test_ipy2d.py
@@ -1,19 +1,27 @@
-from ipy2d import fun
+from ipy2d import convert
 import pytest
 
 
 def test_random_examples():
-    assert fun.from_4("0.0.0.1") == 1, "Should be 1"
-    assert fun.from_6("::1") == 1, "Should be 1"
+    assert convert.from_4("0.0.0.1") == 1, "Should be 1"
+    assert convert.from_6("::1") == 1, "Should be 1"
     assert (
-        fun.from_6("2001:4860:4860::8888") == 42541956123769884636017138956568135816
+        convert.from_6("2001:4860:4860::8888") == 42541956123769884636017138956568135816
     ), "Should be 42541956123769884636017138956568135816"
 
-    assert fun.to_4(5) == "0.0.0.5", "Should be 0.0.0.5"
+    assert convert.to_4(5) == "0.0.0.5", "Should be 0.0.0.5"
     assert (
-        fun.to_6(42541956123769884636017138956568135817)
+        convert.to_6(42541956123769884636017138956568135817)
         == "2001:4860:4860:0000:0000:0000:0000:8889"
     ), "Should be 2001:4860:4860:0000:0000:0000:0000:8889"
+    assert (
+        convert.to_6(0x20014860486000000000000000008889)
+        == "2001:4860:4860:0000:0000:0000:0000:8889"
+    ), "Should be 2001:4860:4860:0000:0000:0000:0000:8889"
+    assert (
+        convert.to_6(convert.from_6("0000:0000:0000:dead:beef:0000:0000:0000"))
+        == "0000:0000:0000:dead:beef:0000:0000:0000"
+    ), "Should be 0000:0000:0000:dead:beef:0000:0000:0000"
 
 
 def test_first_two_octets_ipv4():
@@ -24,28 +32,37 @@ def test_first_two_octets_ipv4():
                 for i4 in ints:
                     i = i1 << 0x18 | i2 << 0x10 | i3 << 0x08 | i4
                     assert (
-                        fun.from_4("{0}.{1}.{2}.{3}".format(i1, i2, i3, i4)) == i
+                        convert.from_4(".".join(map(str, [i1, i2, i3, i4]))) == i
                     ), "Should be {0}".format(i)
-                    assert fun.to_4(i) == "{0}.{1}.{2}.{3}".format(
-                        i1, i2, i3, i4
-                    ), "Should be {0}".format("{0}.{1}.{2}.{3}".format(i1, i2, i3, i4))
+                    assert convert.to_4(i) == ".".join(
+                        map(str, [i1, i2, i3, i4])
+                    ), ".".join(map(str, [i1, i2, i3, i4]))
 
 
 def test_compressed_ipv6():
-    assert "::1" == fun.to_6(1, True), "Should be ::1"
+    assert "::1" == convert.to_6(1, compressed=True), "Should be ::1"
+    assert "2001:4860:4860::8889" == convert.to_6(
+        0x20014860486000000000000000008889, compressed=True
+    ), "Should be 2001:4860:4860::8889"
+    assert "1023::3a:3:0" == convert.to_6(
+        0x1023_0000_0000_0000_0000_003A_0003_0000, compressed=True
+    ), "Should be 1023::3a:3:0"
+    assert "::dead:beef:0:0:0" == convert.to_6(
+        1051570404137199630024704, compressed=True
+    ), "Should be ::dead:beef:0:0:0"
 
 
 @pytest.mark.skip(reason="this test takes to long!")
 def test_all_ipv4():
-    ints = [i for i in range(0, 0x100)]
+    ints = [i for i in range(0x00, 0x100)]
     for i1 in ints:
         for i2 in ints:
             for i3 in ints:
                 for i4 in ints:
                     i = i1 << 0x18 | i2 << 0x10 | i3 << 0x08 | i4
                     assert (
-                        fun.from_4("{0}.{1}.{2}.{3}".format(i1, i2, i3, i4)) == i
+                        convert.from_4(".".join(map(str, [i1, i2, i3, i4]))) == i
                     ), "Should be {0}".format(i)
-                    assert fun.to_4(i) == "{0}.{1}.{2}.{3}".format(
-                        i1, i2, i3, i4
-                    ), "Should be {0}".format("{0}.{1}.{2}.{3}".format(i1, i2, i3, i4))
+                    assert convert.to_4(i) == ".".join(
+                        map(str, [i1, i2, i3, i4])
+                    ), "Should be {0}".format(".".join(map(str, [i1, i2, i3, i4])))
