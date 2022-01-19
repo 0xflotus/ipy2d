3c0b7f73531780d184bea55051cd04d89ad1cceb
0xflotus <0xflotus@gmail.com>
2022-01-19 02:33:18 +0100

    changed variable names

--- ipy2d/fun.py
+++ ipy2d/fun.py
@@ -2,20 +2,23 @@ import netaddr
 import ipaddress
 
 def from_4(ipv4):
-    arr = ipv4.split('.')
-    return int(arr[0]) << 0x18 | int(arr[1]) << 0x10 | int(arr[2]) << 0x08 | int(arr[3]) << 0x00
+    octets = ipv4.split('.')
+    return int(octets[0]) << 0x18 | \
+           int(octets[1]) << 0x10 | \
+           int(octets[2]) << 0x08 | \
+           int(octets[3]) << 0x00
 
 def from_6(ipv6):
     ip = netaddr.IPAddress(ipv6)
-    arr = ip.format(dialect=netaddr.ipv6_verbose).split(':')
-    return int(arr[0], 0x10) << 0x70 | \
-           int(arr[1], 0x10) << 0x60 | \
-           int(arr[2], 0x10) << 0x50 | \
-           int(arr[3], 0x10) << 0x40 | \
-           int(arr[4], 0x10) << 0x30 | \
-           int(arr[5], 0x10) << 0x20 | \
-           int(arr[6], 0x10) << 0x10 | \
-           int(arr[7], 0x10) << 0x00 
+    hextet = ip.format(dialect=netaddr.ipv6_verbose).split(':')
+    return int(hextet[0], 0x10) << 0x70 | \
+           int(hextet[1], 0x10) << 0x60 | \
+           int(hextet[2], 0x10) << 0x50 | \
+           int(hextet[3], 0x10) << 0x40 | \
+           int(hextet[4], 0x10) << 0x30 | \
+           int(hextet[5], 0x10) << 0x20 | \
+           int(hextet[6], 0x10) << 0x10 | \
+           int(hextet[7], 0x10) << 0x00 
 
 def to_4(num):
     return "{0}.{1}.{2}.{3}".format(num >> 0x18 & 0xff, \
